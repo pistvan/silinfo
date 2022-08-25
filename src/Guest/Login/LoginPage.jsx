74ded48a058e0ff5e14c3c7ceb1a4b8f20c35549
Istvan Palinkas <pistvan@pistvan.hu>
2022-08-25 13:38:00 +0200

    login oldal

---
+++ src/Guest/Login/LoginPage.jsx
@@ -0,0 +1,69 @@
+import { Box, Button, Container, createTheme, Link, TextField, ThemeProvider } from "@mui/material";
+import { green } from "@mui/material/colors";
+
+const LoginPage = () => {
+	const theme = createTheme({
+		palette: {
+			primary: {
+				main: green[500],
+				contrastText: 'white',
+			},
+		}
+	});
+
+	return (
+	<ThemeProvider theme={theme}>
+		<Container maxWidth="sm" sx={{
+			borderWidth: '1px',
+			borderColor: green[800],
+			borderStyle: 'solid',
+			borderRadius: '1em',
+			padding: 2,
+			mt: 2,
+		}}>
+			<Box component="form" sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+			}}>
+				<div>Jelentkezz be a felhasználóneveddel és jelszavaddal!</div>
+				<TextField
+					required
+					id="username"
+					name="username"
+					label="Felhasználónév"
+					fullWidth
+					margin="normal"
+					autoFocus
+				/>
+				<TextField
+					required
+					id="password"
+					name="password"
+					label="Jelszó"
+					fullWidth
+					margin="normal"
+					type="password"
+				/>
+				<Button
+					type="submit"
+					variant="contained"
+					color="primary"
+					sx={{
+						mt: 3,
+						mb: 2,
+						width: '50%',
+					}}
+				>
+					Bejelentkezés
+				</Button>
+				<Link>
+					Elfelejtettem a jelszavam
+				</Link>
+			</Box>
+		</Container>
+	</ThemeProvider>
+	);
+}
+
+export default LoginPage;
